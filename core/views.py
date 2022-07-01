1c75c0c3fedc1f2f3cf8d6c1b87ea40e4998b2e1
Erick Hernandez <yerickk8@gmail.com>
2022-07-01 15:50:15 -0500

    Added Account Settings

--- core/views.py
+++ core/views.py
@@ -1,14 +1,44 @@
+import re
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
 
-
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')     
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -26,20 +56,42 @@ def signup(request):
                 messages.info(request, 'Username Taken')
                 return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, password=password, email=email)
+                user = User.objects.create_user(
+                    username=username, password=password, email=email)
                 user.save()
 
-                #log user in direct to settings page
+                # log user in direct to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
-                #user a profile object for the new user
+                # user a profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('login')
-
-
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('/')
+    return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout (request):
+    auth.logout(request)
+    return redirect('signin')
